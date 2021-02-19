d51d2d3a758742ef32a4c94907b32dc211028b8d
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-19 09:54:49 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LesConditionsExo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Créer une variable rainy puis lui attribuer une valeur booléenne.
+            //Dans la console, selon la valeur de rainy, afficher un des messages suivants:
+            //« Bonjour, il ne pleut pas aujourd’hui, bonne journée. »
+            //« Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie. »
+
+
+            bool rainy = true;
+
+            if(rainy)
+            {
+                Console.WriteLine("Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie");
+            }                
+                else
+            {
+                Console.WriteLine(" Bonjour, il ne pleut pas aujourd’hui, bonne journée");
+            }
+        }
+    }
+}
