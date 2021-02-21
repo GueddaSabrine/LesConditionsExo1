ee5d4e158c9a301d65d2317ef8fc689745cbd42d
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2021-02-21 17:41:01 +0100

    exo1

--- Program.cs
+++ Program.cs
@@ -4,14 +4,13 @@ namespace LesConditionsExo1
 {
     class Program
     {
+
+/*Créer une variable rainy puis lui attribuer une valeur booléenne.
+            Dans la console, selon la valeur de rainy, afficher un des messages suivants:
+            « Bonjour, il ne pleut pas aujourd’hui, bonne journée. »
+            « Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie. »*/
         static void Main(string[] args)
         {
-            //Créer une variable rainy puis lui attribuer une valeur booléenne.
-            //Dans la console, selon la valeur de rainy, afficher un des messages suivants:
-            //« Bonjour, il ne pleut pas aujourd’hui, bonne journée. »
-            //« Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie. »
-
-
             bool rainy = true;
 
             if(rainy)
